40e8104c27f49d3dcc739ee20d727678f39cf882
Raul125 <raul125esp@gmail.com>
2025-02-24 19:37:08 +0100

    Anti Door Stuck

--- Plataforma/Assets/Scripts/Rooms/Door.cs
+++ Plataforma/Assets/Scripts/Rooms/Door.cs
@@ -10,14 +10,17 @@ public class Door : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
             if (collision.transform.position.x < transform.position.x)
             {
+                rb.AddForce(Vector2.right * 20, ForceMode2D.Impulse);
                 cameraController.MoveToNewRoom(nextRoom);
                 nextRoom.GetComponent<Room>().ActivateRoom(true);
                 previousRoom.GetComponent<Room>().ActivateRoom(false);
             }
             else
             {
+                rb.AddForce(Vector2.left * 20, ForceMode2D.Impulse);
                 cameraController.MoveToNewRoom(previousRoom);
                 previousRoom.GetComponent<Room>().ActivateRoom(true);
                 nextRoom.GetComponent<Room>().ActivateRoom(false);
